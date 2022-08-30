46bab683d55fa417dcd4e7dc91c070633a404d3e
Piyush Mohapatra <piyush.ninja@gmail.com>
2022-08-30 20:39:08 +0530

    Final Balloon detection

--- ballon_detection/import cv2.py
+++ ballon_detection/import cv2.py
@@ -1,18 +1,39 @@
 import cv2
-import sys
+import numpy as np
+import imutils
+import matplotlib.pyplot as plt
+cap = cv2.VideoCapture('/home/piyush/Tracking/vid.mp4')
+redLower = np.array([0,10,170], dtype='uint8')
+redUpper = np.array([50,50,255], dtype='uint8')
+c = 0
+frame_width = int(cap.get(3)) 
+frame_height = int(cap.get(4)) 
+   
+size = (frame_width, frame_height) 
 
-(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
- 
-if __name__ == '__main__' :
-    tracker_type = 'MIL'
- 
-    if int(minor_ver) < 3:
-        tracker = cv2.TrackerMIL_create()
-video = cv2.VideoCapture("vid.mp4")
-if not video.isOpened():
-  print("Could not open video")
-  sys.exit()
-ok, frame = video.read()
-if not ok:
-  print ('Cannot read video file')
-  sys.exit()
+result = cv2.VideoWriter('balltracking.avi',cv2.VideoWriter_fourcc(*'MPEG'),10, size) 
+while True:
+    grapped,frame=cap.read()
+    if grapped == True:
+        
+        red = cv2.inRange(frame,redLower,redUpper)
+        red = cv2.GaussianBlur(red,(3,3),0)
+
+        cnts = cv2.findContours(red.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        if len(cnts) > 0:
+            cnt = sorted(cnts,key=cv2.contourArea,reverse=True)[0]
+            rect = np.int32(cv2.boxPoints(cv2.minAreaRect(cnt)))
+            cv2.circle(frame, (rect[0][0]+(rect[-1][0] - rect[0][0])//2,rect[1][1]+(rect[-1][-1]-rect[1][1])//2), 2, (0, 0, 0), 1)
+        #cv2.imshow("Ball Tracking", frame)
+        result.write(frame)
+        #if cv2.waitKey() & 0xFF == ord("q"):
+         #   break
+        
+    else:
+        break
+        
+
+cap.release()
+cv2.destroyAllWindows()
